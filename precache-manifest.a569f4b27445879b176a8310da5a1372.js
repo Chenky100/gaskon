da87ba274da3326fb390a6e266163afea56c5848
Chenky100 <chenky100@gmail.com>
2020-10-07 03:28:49 +0300

    Updates

---
+++ precache-manifest.a569f4b27445879b176a8310da5a1372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a580f2ff7da5d72933d36e7ca359ea",
+    "url": "/BestMovies/index.html"
+  },
+  {
+    "revision": "18922684888aa8586ac7",
+    "url": "/BestMovies/static/css/main.f5b815ac.chunk.css"
+  },
+  {
+    "revision": "ec8d678b0c886e2f409d",
+    "url": "/BestMovies/static/js/2.e9bd84aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BestMovies/static/js/2.e9bd84aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18922684888aa8586ac7",
+    "url": "/BestMovies/static/js/main.cb90b694.chunk.js"
+  },
+  {
+    "revision": "5ad475f296d54aa53ab9",
+    "url": "/BestMovies/static/js/runtime-main.643a53a4.js"
+  },
+  {
+    "revision": "5e0f5adfda903afb93b5ffe04f3ca570",
+    "url": "/BestMovies/static/media/logo.5e0f5adf.jpg"
+  }
+]);
